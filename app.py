04672da585a100374223cc17bd41ddeb0da98cbe
Anugrah Akbar P <workspaceanugrah@gmail.com>
2023-01-10 01:29:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
 model_name = 'anugrahap/gpt2-indo-textgen'
-model = AutoModelForCausalLM.from_pretrained(model_name, pad_token_id=tokenizer.eos_token_id)
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='left')
+model = AutoModelForCausalLM.from_pretrained(model_name, pad_token_id=tokenizer.eos_token_id)
 
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 prompt_text = st.text_input(label = "Enter your prompt text...")
